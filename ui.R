0a9e1cdd5e9bb11b51ee4e4218050e9193c6c33f
Trevor Gast <shytor@hotmail.com>
2015-10-21 21:38:36 -0400

    instructions
    added instructions, labels

--- ui.R
+++ ui.R
@@ -4,9 +4,11 @@ shinyUI(pageWithSidebar(
     sidebarPanel(
         numericInput('val1', 'How many values?', 50, min = 50, max = 150, step = 10),
         numericInput('sd1', 'Standard Deviation', 10, min = 1, max = 20, step = 1),
-        h3('Use the slider to guess the mu value'),
-        sliderInput('mu', 'Guess the mean',value = 70, min = 65, max = 75, step = 0.05,),
-        actionButton("ansButton", "Get Answer")
+        sliderInput('mu','Use this slider to guess the mean',value = 70, min = 65, max = 75, step = 0.05,),
+        actionButton("ansButton", "Get Answer"),
+        h3('Instructions:'),
+        p('Adjusting the values or the standard deviation creates a new set of random (normal) data values. You may choose from 50 to 150 values with standard deviation from 1 to 20.'),
+        p('Then, use the slider to guess the mean of the data set. Click the Get Answer button to check your guess!')
     ),
     mainPanel(
         h3('Histogram of Random values'),
